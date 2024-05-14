1bb9ba0e3293b607b1b0f7d7a2d55b4d9edf8490
DuduGuima <eduardo.guima.dudu@gmail.com>
2024-05-14 17:04:55 +0200

    arnold into GMRes

--- MyGMRES.jl
+++ MyGMRES.jl
@@ -23,16 +23,31 @@ function Arnold(A,n,b=A[:,1])
 end
 
 function MyGMRES(A,b,maxiter=size(A,2))
+    Q=zeros(size(A,1),maxiter)
+    H=zeros(maxiter,maxiter)
     x=zeros(size(b))
     bheta = norm(b)
+    v = b/bheta
+    Q[:,1] = v
     for k=1:maxiter
-        Q,H=Arnold(A,k,b)#melhorar essa insercao de matrizes
+        #Q,H=Arnold(A,k,b)#melhorar essa insercao de matrizes
+        ###Arnold's iteration inside GMRES to use Q,H from past iterations
+        if k>1
+            Q[:,k] = v/H[k,k-1]
+        end
+        v = A*Q[:,k]
+        for j=1:k
+            H[j,k] = (Q[:,j]')*v
+            v-= H[j,k]*Q[:,j]
+        end
+        if k<maxiter
+            H[k+1,k]=norm(v)
+        end
+
         e1=Matrix(I,k,1)
-        y = H\(bheta*e1)        
-        x=Q*y
-        #println(x)
-        # println("size of y\n",size(y))
-        # println("y\n",y)
+        y = H[1:k,1:k]\(bheta*e1)        
+        x=Q[:,1:k]*y
+        
         if norm(A*x - b) < 0.001#ta zoado esse calculo aqui tb
             return x
         end
